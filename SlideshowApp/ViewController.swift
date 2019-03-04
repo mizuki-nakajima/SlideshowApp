99942c62b3444ea7b95a04efdcc2e3e45e7385fb
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-03-04 23:25:00 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Nakajima Mizuki on 2019/03/04.
+//  Copyright © 2019 Nakajima Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
